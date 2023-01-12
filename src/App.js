64aab87ab819215c2402e6c87edb4f18c74a0338
Azeem <livelyazeem@gmail.com>
2023-01-12 15:03:28 +0500

    learning chap 8

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header title="Gorcery List" />
       <Content />
       <Footer />
     </div>
